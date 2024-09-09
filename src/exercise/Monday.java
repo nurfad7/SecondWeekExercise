333975cc209e7898ab0bb40598aaa8c592cfa7f4
nurfad7 <nurfad7@gmail.com>
2024-09-09 16:24:13 +0700

    feat: add monday exercise number 1

--- src/exercise/Monday.java
+++ src/exercise/Monday.java
@@ -1,5 +1,9 @@
 package exercise;
+import tools.ScannerCommandBuilder;
+
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Monday {
     private final Scanner scanner;
@@ -9,6 +13,31 @@ public class Monday {
     }
 
     public void result() {
+        //#1 Java Array Program For Array Rotation
+        System.out.println("Let's input the array first.");
+        ArrayList<Integer> arrayToRotate = ScannerCommandBuilder
+                .getIntegerArrayFromUserInput(scanner);
+        int d = ScannerCommandBuilder
+                .getIntegerFromUserInput(scanner,"Enter d: ");
+        System.out.println(rotateArrayCustom(arrayToRotate, d).toString());
+        //#2
+    }
 
+    private ArrayList<Integer> rotateArrayCustom(ArrayList<Integer> arrayToRotate, int d) {
+        ArrayList<Integer> rotatedArray = new ArrayList<>();
+        ArrayList<Integer> beforeLimitArray = new ArrayList<>();
+        AtomicBoolean isStartToRotate = new AtomicBoolean(false);
+        arrayToRotate.forEach(number -> {
+            if (!isStartToRotate.get() && d == number) {
+                beforeLimitArray.add(number);
+                isStartToRotate.set(true);
+            } else if(!isStartToRotate.get()) {
+                beforeLimitArray.add(number);
+            } else {
+                rotatedArray.add(number);
+            }
+        });
+        rotatedArray.addAll(beforeLimitArray);
+        return rotatedArray;
     }
 }
---
+++ src/tools/ScannerCommandBuilder.java
@@ -0,0 +1,65 @@
+package tools;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ScannerCommandBuilder {
+    public static String getStringFromUserInput(Scanner scanner, String description) {
+        System.out.print(description);
+        return scanner.nextLine();
+    }
+
+    public static int getIntegerFromUserInput(Scanner scanner, String description) {
+        boolean isTypedCorrectly = false;
+        int number = 0;
+        do {
+            System.out.print(description);
+            try {
+                number = scanner.nextInt();
+                isTypedCorrectly = true;
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                System.out.println("Invalid input. Please input number");
+            }
+        } while (!isTypedCorrectly);
+        return number;
+    }
+
+    public static double getDoubleFromUserInput(Scanner scanner, String description) {
+        boolean isTypedCorrectly = false;
+        double number = 0;
+        do {
+            System.out.print(description);
+            try {
+                number = scanner.nextDouble();
+                isTypedCorrectly = true;
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                System.out.println("Invalid input. Please input number");
+            }
+        } while (!isTypedCorrectly);
+        return number;
+    }
+
+    public static ArrayList<Integer> getIntegerArrayFromUserInput(Scanner scanner) {
+        boolean isTypedCorrectly = false;
+        System.out.print("Enter array length: ");
+        int arrayLength = scanner.nextInt();
+        ArrayList<Integer> numbers = new ArrayList<>();
+        do {
+            System.out.print("Enter number: ");
+            try {
+                if(numbers.size() == arrayLength) {
+                    isTypedCorrectly = true;
+                    continue;
+                }
+                numbers.add(scanner.nextInt());
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                System.out.println("Invalid input. Please input number");
+            }
+        } while (!isTypedCorrectly);
+        return numbers;
+    }
+}
